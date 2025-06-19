f223e487425d06ab7d8b2c34acd4ee457a831d83
JoaoArtur <uc2019217853@student.uc.pt>
2025-06-19 14:45:21 +0200

    chore: setup components

---
+++ customer-manager/src/test/java/com/j_market/customer_manager/CustomerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j_market.customer_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ pos-terminal/src/test/java/com/j_market/pos_terminal/PosTerminalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j_market.pos_terminal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosTerminalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
